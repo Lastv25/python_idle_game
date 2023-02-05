21999a3ffbf1f887699012a4e6a5ffc6b8572c4c
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2023-02-05 17:36:21 +0100

    fix text

--- src/components/main_screen/main_screen.py
+++ src/components/main_screen/main_screen.py
@@ -5,20 +5,25 @@ class MainScreen(Screen):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.text_counter = 0
-        self.row_limit = 5
         self.time_text = 0.01
+        self.characters_per_row = 56
 
     
     def add_text(self, text):
+        print(text)
         self.ids.label.text += text
-        self.text_counter += 1
-        self.ids.scroll_view.scroll_y = 0
+        self.ids.scroll_view.scroll_y = 1
 
     def text_init(self):
         with open('src/assets/texts/lorem_ipsum.txt', 'r') as file:
-            for row in range(self.row_limit):
-                line = file.readline()
-                self.add_text(line)
+            for row in file:
+                len_row = len(row)
+                charac_count = 0
+                while charac_count < len_row:
+                    init = charac_count
+                    end = self.characters_per_row+charac_count
+                    self.add_text(row[init:end]+"\n")
+                    charac_count += self.characters_per_row
+                time.sleep(self.time_text)
 
 
